e74f75e3b8073e51b125d6334ce94c37f90d765b
Vinh <beminhquan3@gmail.com>
2020-04-07 17:16:38 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../models/userModel');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:userID', function(req, res, next) {
+  const userID = req.params.userID;
+  User.findById(userID,function(err,user){
+      if(err){
+        res.json({
+          status: 'err',
+          code: 500,
+          message: err
+        })
+      }
+      res.status(200).json({
+        message: 'thanhcong',
+        data: user
+      })
+  });
+
+//   User.findById( userID , function(err,user){
+//     if(err){
+//         res.json({
+//             status:'err',
+//             code: 500,
+//             message: err
+//         })
+//     }
+//     res.json({
+//         status: 'success',
+//         code: 200,
+//         message: 'Registros encontrado',
+//         data: user.product
+//     })
+// })
 });
 
 module.exports = router;
