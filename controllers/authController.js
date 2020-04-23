d24ed0c2e0a5dc166040b0071cb4f5dfde465af3
Vinh <beminhquan3@gmail.com>
2020-04-23 20:26:53 +0700

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,7 +20,6 @@ const storage = multer.diskStorage({
         cb(null,  Date.now() +  file.originalname);
     }
     });
-    
     const fileFilter = (req,file, cb) => {
         if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' ||file.mimetype === 'image/jpg'){
             cb(null, true);
