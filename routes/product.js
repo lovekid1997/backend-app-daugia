2a7d03f771217e9006505c4f4c1156cf95641196
Vinh <beminhquan3@gmail.com>
2020-05-23 21:45:51 +0700

    update

--- routes/product.js
+++ routes/product.js
@@ -124,7 +124,9 @@ router.post('/new/:userID',upload.array('imageProduct',5),(req,res,next)=>{
         })
     } catch (e) {
         console.log(e)
-        res.status(500).send(e);
+        res.status(500).json({
+            message : e
+        })
     }
 })
 
